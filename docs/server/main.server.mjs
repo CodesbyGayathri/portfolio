826bea70cc38ef8f3f6b2be1c02c8620b42b38af
CodesbyGayathri <codesbygj@gmail.com>
2025-01-09 18:02:14 -0600

    UI changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VGUNPW23.mjs";import"./chunk-ER3L6EIM.mjs";import"./chunk-7FOSXBQY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-U46XROOS.mjs";import"./chunk-IGJ35QEI.mjs";import"./chunk-S5YLHOIN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
